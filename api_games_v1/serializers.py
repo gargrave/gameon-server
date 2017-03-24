8f069dc4c6a5305e3a92b5d2ff1dff6be2d397e2
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-03-24 10:24:54 -0700

    Separate Game serializers for read/write operations

--- api_games_v1/serializers.py
+++ api_games_v1/serializers.py
@@ -58,21 +58,85 @@ class TagGameRelationReadSerializer(serializers.ModelSerializer):
 
 
 ########################################################
+# Platform Serializers
+########################################################
+class PlatformSerializer(serializers.ModelSerializer):
+    """
+    Basic serializer for Platform model.
+    """
+    class Meta:
+        model = PlatformModel
+        fields = ('id', 'title', 'created', 'modified')
+
+
+class PlatformNestedSerializer(serializers.ModelSerializer):
+    """
+    Basic serializer for Platform model, when used in the Games read views.
+    This simply limits the information that is sent with Games to
+    what is necessary. (i.e. no created/modified info, etc.)
+    """
+    class Meta:
+        model = PlatformModel
+        fields = ('id', 'title')
+
+
+########################################################
 # Game Serializers
 ########################################################
-class GameSerializer(serializers.ModelSerializer):
-    # use the custom serializer to build a tag list
-    tags = TagGameRelationReadSerializer(many=True, read_only=True)
+class GameReadSerializer(serializers.ModelSerializer):
+    """
+    Basic serializer for viewing/list Game model.
+
+    This serializer has the following customizations:
+    - Dates list is populated as a list of string with the
+        associated dates values
+    - Platform instance is populated using PlatformNestedSerializer
+        because we want to return full Platform data, rather than just the ID.
+    """
+    dates = serializers.StringRelatedField(many=True, read_only=True)
+    platform = PlatformNestedSerializer(read_only=True)
 
     class Meta:
         model = Game
-        fields = ('id', 'title', 'tags',
+        fields = ('id', 'title', 'platform', 'dates',
                   'created', 'modified')
 
+
+class GameWriteSerializer(serializers.ModelSerializer):
+    """
+    Basic serializer for creating/editing Game model.
+
+    This serializer provides a custom to_representation implementation
+        in order to correctly populate the 'platform' and 'dates' fields
+        for the return value after a write request.
+    """
+
+    class Meta:
+        model = Game
+        fields = ('id', 'title', 'platform', 'created', 'modified')
+
+    def to_representation(self, obj):
+        platform = PlatformModel.objects.get(pk=obj.platform)
+        dates = [str(d) for d in GameDateRelation.objects.filter(
+            owner=obj.owner,
+            game__title=obj.title)]
+        return {
+            'id': obj.id,
+            'title': obj.title,
+            'platform': {
+                'id': platform.id,
+                'title': platform.title,
+            },
+            'dates': dates,
+            'created': obj.created,
+            'modified': obj.modified,
+        }
 
 ########################################################
 # User/Auth Serializers
 ########################################################
+
+
 class UserProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
