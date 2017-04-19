ca5a64c8e52ca0f3a916cda9fabc8a97b82f31e2
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-04-19 09:11:54 -0700

    Update URLs and views for 'Games' API endpoint

--- api_games_v1/urls.py
+++ api_games_v1/urls.py
@@ -5,15 +5,8 @@ from . import views_games, views_platforms, views_tags
 urlpatterns = [
     url(r'^games/?$', views_games.GameList.as_view()),
     url(r'^games/(?P<pk>[0-9]+)/?$', views_games.GameDetail.as_view()),
-    url(r'^games/create/?$', views_games.GameCreate.as_view()),
-    url(r'^games/update/(?P<pk>[0-9]+)/?$', views_games.GameUpdate.as_view()),
 
     url(r'^platforms/?$', views_platforms.PlatformList.as_view()),
     url(r'^platforms/(?P<pk>[0-9]+)/?$',
-        views_platforms.PlatformDetail.as_view()),
-
-    url(r'^tags/?$', views_tags.TagList.as_view()),
-    url(r'^tags/add/?$', views_tags.TagGameRelationCreateView.as_view()),
-    url(r'^tags/remove/?$', views_tags.TagGameRelationDeleteView.as_view()),
-    url(r'^tags/delete/(?P<pk>[0-9]+)/?$', views_tags.TagDeleteView.as_view()),
+        views_platforms.PlatformDetail.as_view())
 ]
--- api_games_v1/views_games.py
+++ api_games_v1/views_games.py
@@ -3,80 +3,103 @@ Views for the 'games' API endpoints.
 """
 from rest_framework import generics, permissions
 
-from .models import Game, GameDateRelation
+from .models import Game, GameDateRelation, PlatformModel
 from .serializers import GameReadSerializer, GameWriteSerializer
 
 
-class GameList(generics.ListAPIView):
+def parse_platform(request):
+    """
+    Returns a PlatformModel instance based on the ID specified in the request.
+    """
+    return PlatformModel.objects.get(
+        owner=request.user,
+        id=request.data.get('platformId'))
+
+
+def process_dates(request, game, dates):
+    """
+    Processes the list of new dates to add to a Game instance.
+    Simply run a get_or_create() call with each provided date,
+        just to make sure that that date exists.
+    """
+    if dates:
+        for date in dates:
+            GameDateRelation.objects.get_or_create(
+                owner=request.user,
+                date=date,
+                game=game,
+            )
+
+
+def process_removed_dates(request, game, dates):
+    """
+    Processes the list of new dates to add to a Game instance.
+    Simply run a get() call with each provided date,
+        and delete any that are found.
+    """
+    if dates:
+        for date in dates:
+            gdr = GameDateRelation.objects.get(
+                owner=request.user,
+                date=date,
+                game=game,
+            )
+            if gdr:
+                gdr.delete()
+
+
+class GameList(generics.ListCreateAPIView):
     """
     Concrete view for listing a queryset or creating a model instance.
     """
     permission_classes = (permissions.IsAuthenticated,)
-    serializer_class = GameReadSerializer
 
-    def get(self, request, *args, **kwargs):
-        self.queryset = Game.objects.filter(owner=self.request.user)
-        return self.list(request, *args, **kwargs)
+    def get_queryset(self):
+        return Game.objects.filter(owner=self.request.user)
 
-
-class GameDetail(generics.RetrieveAPIView):
-    """
-    Concrete view for retrieving a model instance.
-    """
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = Game.objects.all()
-    serializer_class = GameReadSerializer
-
-
-class GameCreate(generics.CreateAPIView):
-    """
-    Concrete view for creating a model instance.
-
-    Adding a custom perform_create implementation to add GameDateRelations
-    for any necessary dates provided with this request.
-    """
-    permission_classes = (permissions.IsAuthenticated,)
-    serializer_class = GameWriteSerializer
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return GameReadSerializer
+        if self.request.method == 'POST':
+            return GameWriteSerializer
 
     def perform_create(self, serializer):
-        game_instance = serializer.save(owner=self.request.user)
-        date_values = self.request.data.get('dates')
-        for date in date_values:
-            GameDateRelation.objects.get_or_create(
-                owner=self.request.user,
-                date=date,
-                game=game_instance,
-            )
+        game_instance = serializer.save(
+            owner=self.request.user,
+            platform=parse_platform(self.request))
+        # create any new dates in the request
+        process_dates(self.request, game_instance,
+                      self.request.data.get('dates'))
 
 
-class GameUpdate(generics.UpdateAPIView):
+class GameDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Concrete view for updating a model instance.
+    Concrete view for retrieving, updating or deleting a model instance.
     """
     permission_classes = (permissions.IsAuthenticated,)
-    queryset = Game.objects.all()
-    serializer_class = GameWriteSerializer
+
+    def get_queryset(self):
+        return Game.objects.filter(owner=self.request.user)
+
+    def get_serializer_class(self):
+        if self.request.method in ('GET', 'DELETE'):
+            return GameReadSerializer
+        if self.request.method in ('PUT', 'PATCH'):
+            return GameWriteSerializer
 
     def perform_update(self, serializer):
-        game_instance = serializer.save()
-        date_values = self.request.data.get('dates')
-        removed_dates = self.request.data.get('datesRemoved')
+        # add platform info IF an ID is provided; if not, just leave it blank
+        # this is to support both PUT and PATCH methods,
+        # where the supplied data may be incomplete
+        if self.request.data.get('platformId'):
+            game_instance = serializer.save(
+                platform=parse_platform(self.request))
+        else:
+            game_instance = serializer.save()
 
         # create any new dates in the request
-        if date_values:
-            for date in date_values:
-                GameDateRelation.objects.get_or_create(
-                    owner=self.request.user,
-                    date=date,
-                    game=game_instance,
-                )
-
+        process_dates(self.request, game_instance,
+                      self.request.data.get('dates'))
         # delete any dates that have been removed
-        if removed_dates:
-            for date in removed_dates:
-                gdr = GameDateRelation.objects.get(
-                    owner=self.request.user,
-                    date=date,
-                    game=game_instance,
-                )
-                gdr.delete()
+        process_removed_dates(self.request, game_instance,
+                              self.request.data.get('datesRemoved'))
